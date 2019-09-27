0458cf884d30fd7189fc44e129e56a8c16388743
mohanpratapa <pvskrishnamohan24@gmail.com>
2019-09-27 11:51:00 -0500

    GPA challenge

---
+++ main.js
@@ -0,0 +1,45 @@
+
+
+const average = (x, y,z) => { return (x + y + z)/3 }
+
+const validate = async (event) => {
+  console.log(`triggered validate on ${event.target.id}`)
+  const isValid = event.target.checkValidity()
+  if (isValid) {
+    event.target.nextElementSibling.innerHTML = ''
+  } else {
+    event.target.nextElementSibling.innerHTML = 'Invalid input'
+    event.target.focus()
+  }
+}
+
+const updateWithAdd = async (event) => {
+  document.querySelector('#result').innerHTML = ''
+  if (document.querySelector('#firstNumber').checkValidity() && document.querySelector('#secondNumber').checkValidity()) {
+    const regex = /[^a-zA-Z_]/g
+    const s = document.querySelector('#guest').value.replace(regex, '')
+    const i = parseInt(document.querySelector('#firstNumber').value)
+    const j = parseInt(document.querySelector('#secondNumber').value)
+    const k = parseInt(document.querySelector('#thirdNumber').value)
+    const ans = `${s}, your average is ${average(i, j, k)}.`
+    document.querySelector('#result').innerHTML = ans
+  }
+}
+
+
+
+// delegate to dynamic elements (e.g. when testing)
+// focusout is like blur, but it bubbles up
+
+document.addEventListener('focusout', event => {
+  if ((event.target && event.target.id === 'firstNumber') ||
+    (event.target && event.target.id === 'secondNumber')) {
+    validate(event)
+  }
+})
+
+document.addEventListener('click', event => {
+  if (event.target && event.target.id === 'addButton') { updateWithAdd(event) }
+})
+
+
